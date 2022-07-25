fca71fc04ebab53facd8b750c4c62b1d49f9e2c0
ValikKabanchenko <kabanchenkoval@gmail.com>
2022-07-25 17:46:50 +0300

    Регистрация и настройка textFeild
    Сделал что бы text Field  проверял есть ли в массиве такой номер или нету

---
+++ ConfirmTouchIDViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ConfirmTouchIDViewController.swift
+//  MegapolicApp
+//
+//  Created by Валик Кабанченко on 25.07.2022.
+//
+
+import UIKit
+
+class ConfirmTouchIDViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
